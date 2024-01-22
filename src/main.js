d4388de0323a5c256f07db1e4a71a81c306b379e
keneth217 <kipyegonkeneth03@gmail.com>
2024-01-22 17:51:25 +0300

    styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
